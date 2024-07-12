6699888e624ce0d9fffcfb6e4db178cdf7b4d531
¨Mario <marioborjac58@gmail.com>
2024-07-12 15:55:04 -0500

    added second dataframe

---
+++ src/data/data_prep.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy as sp
+
+# Crear un diccionario con algunos datos
+data = {
+    'Nombre': ['Ana', 'Juan', 'María', 'Luis'],
+    'Edad': [23, 21, 25, 22],
+    'Ciudad': ['Madrid', 'Barcelona', 'Valencia', 'Sevilla']
+}
+
+# Crear el DataFrame utilizando el diccionario
+df = pd.DataFrame(data)
