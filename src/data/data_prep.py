4b774d20ebfee376715141a148142ca0069ccc9c
¨Mario <marioborjac58@gmail.com>
2024-07-12 19:12:12 -0500

    new feature realese

--- src/data/data_prep.py
+++ src/data/data_prep.py
@@ -11,5 +11,18 @@ data = {
     'Ciudad': ['Madrid', 'Barcelona', 'Valencia', 'Sevilla']
 }
 
+data2 = {
+    'Nombre': ['Mario', 'Elvis', 'Domenica', 'Charlotte'],
+    'Edad': [23, 21, 25, 22],
+    'Ciudad': ['Madrid', 'Barcelona', 'Valencia', 'Sevilla']
+}
+
 # Crear el DataFrame utilizando el diccionario
 df = pd.DataFrame(data)
+
+df2 = pd.DataFrame(data2)
+
+#new feature 
+
+print('Probando llas ramas secundarias de Git, sin trabajar en el main')
+
